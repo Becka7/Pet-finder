ac6e0b27aebde4cb0a5969c56f1923f46b5cb86e
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-09-05 14:14:57 +0300

    imported more images to drawables

--- app/src/main/java/com/moringaschool/petfinder/MainActivity.java
+++ app/src/main/java/com/moringaschool/petfinder/MainActivity.java
@@ -2,13 +2,33 @@ package com.moringaschool.petfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.petsbutton) Button mPetsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mPetsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+//        Toast.makeText(MainActivity.this, "Becka", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this, PetformActivity.class);
+        startActivity(intent);
+
     }
 }
+
