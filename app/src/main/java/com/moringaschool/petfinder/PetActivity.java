2252e8a837f5c918a27ecb374b6375ebe9a8ab2b
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-09-05 14:15:36 +0300

    added a view onclicklistaner interface

---
+++ app/src/main/java/com/moringaschool/petfinder/PetActivity.java
@@ -0,0 +1,26 @@
+package com.moringaschool.petfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PetActivity extends AppCompatActivity {
+//    @BindView(R.id.NameTextView) Button mNameTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet);
+        ButterKnife.bind(this);
+
+//        Intent intent = getIntent();
+//        String name = intent.getStringExtra("name");
+//        mNameTextView.setText("Hey"+name+"here are the pets we chose");
+    }
+}
