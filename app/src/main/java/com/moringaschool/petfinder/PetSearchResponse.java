4ae4c3bb676ed82805c23a9825ec94558d4f406d
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-09-14 21:08:00 +0300

    convert api response from jason to POJO

--- app/src/main/java/com/moringaschool/petfinder/PetSearchResponse.java
+++ app/src/main/java/com/moringaschool/petfinder/PetSearchResponse.java
@@ -1,11 +1,11 @@
 
 package com.moringaschool.petfinder;
 
-import javax.annotation.Generated;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-@Generated("jsonschema2pojo")
+
 public class PetSearchResponse {
 
     @SerializedName("bred_for")
@@ -42,27 +42,7 @@ public class PetSearchResponse {
     @Expose
     private Weight weight;
 
-    /**
-     * No args constructor for use in serialization
-     * 
-     */
-    public PetSearchResponse() {
-    }
 
-    /**
-     * 
-     * @param image
-     * @param bredFor
-     * @param lifeSpan
-     * @param origin
-     * @param temperament
-     * @param name
-     * @param weight
-     * @param id
-     * @param referenceImageId
-     * @param breedGroup
-     * @param height
-     */
     public PetSearchResponse(String bredFor, String breedGroup, Height height, Integer id, Image image, String lifeSpan, String name, String origin, String referenceImageId, String temperament, Weight weight) {
         super();
         this.bredFor = bredFor;
@@ -82,88 +62,44 @@ public class PetSearchResponse {
         return bredFor;
     }
 
-    public void setBredFor(String bredFor) {
-        this.bredFor = bredFor;
-    }
-
     public String getBreedGroup() {
         return breedGroup;
-    }
-
-    public void setBreedGroup(String breedGroup) {
-        this.breedGroup = breedGroup;
     }
 
     public Height getHeight() {
         return height;
     }
 
-    public void setHeight(Height height) {
-        this.height = height;
-    }
-
     public Integer getId() {
         return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public Image getImage() {
         return image;
     }
 
-    public void setImage(Image image) {
-        this.image = image;
-    }
-
     public String getLifeSpan() {
         return lifeSpan;
-    }
-
-    public void setLifeSpan(String lifeSpan) {
-        this.lifeSpan = lifeSpan;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getOrigin() {
         return origin;
-    }
-
-    public void setOrigin(String origin) {
-        this.origin = origin;
     }
 
     public String getReferenceImageId() {
         return referenceImageId;
     }
 
-    public void setReferenceImageId(String referenceImageId) {
-        this.referenceImageId = referenceImageId;
-    }
-
     public String getTemperament() {
         return temperament;
-    }
-
-    public void setTemperament(String temperament) {
-        this.temperament = temperament;
     }
 
     public Weight getWeight() {
         return weight;
     }
-
-    public void setWeight(Weight weight) {
-        this.weight = weight;
-    }
-
 }
+
