b2f95cfcff25866e693959684d56dac2ea9e6387
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-09-14 21:08:29 +0300

    add api base url

--- app/src/main/java/com/moringaschool/petfinder/Weight.java
+++ app/src/main/java/com/moringaschool/petfinder/Weight.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-@Generated("jsonschema2pojo")
+
 public class Weight {
 
     @SerializedName("imperial")
