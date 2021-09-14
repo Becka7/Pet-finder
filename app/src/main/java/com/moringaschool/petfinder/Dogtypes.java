10aa53d92f337c0dc18e9d72cbcef9f970c01536
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-09-15 01:47:23 +0300

    add butterknife and view onclick listener

--- app/src/main/java/com/moringaschool/petfinder/Dogtypes.java
+++ app/src/main/java/com/moringaschool/petfinder/Dogtypes.java
@@ -2,8 +2,10 @@ package com.moringaschool.petfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -12,14 +14,15 @@ import android.widget.Toast;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class Dogtypes extends AppCompatActivity  {
-//    @BindView(R.id.listView) ListView mListView;
+public class Dogtypes extends AppCompatActivity {
+    @BindView(R.id.listView) ListView mListView;
     @BindView(R.id.nameTextView) TextView mNameTextView;
 //    private String[] dogs = new String[]{"Bulldog", "German shepherd", "poodle", "Labrador", "Golden retriver", "Chihuahua", "Rottweiler", "Siberian Husky", "Cavalier king", "Italian greyhound", "Papillon", "Bulldog", "German shepherd", "poodle", "Labrador", "Golden retriver", "Chihuahua", "Rottweiler", "Siberian Husky", "Cavalier king", "Italian greyhound", "Papillon"};
 
@@ -27,12 +30,10 @@ public class Dogtypes extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dogtypes);
-//        ButterKnife.bind(this);
-        ListView listView = (ListView) findViewById(R.id.listView);
+        ButterKnife.bind(this);
 
+//        mListView.setOnClickListener(this);
 
-//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, dogs);
-//        mListView.setAdapter(adapter);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Api.BASE_URL)
@@ -55,7 +56,7 @@ public class Dogtypes extends AppCompatActivity  {
                     petNames[i] = petSearchResponses.get(i).getBreedName();
                 }
 
-                listView.setAdapter(
+                mListView.setAdapter(
                         new ArrayAdapter<String>(
                                 getApplicationContext(),
                                 android.R.layout.simple_list_item_1,
@@ -72,20 +73,7 @@ public class Dogtypes extends AppCompatActivity  {
         });
 
 
-
-
-//        Intent intent = getIntent();
-//        String name = intent.getStringExtra("name");
-//        mNameTextView.setText("Hey"+ name +"Here are some dogs for you");
-
-//        listView.setOnItemClickListener(this) ;
-
         }
 
-//        @Override
-//        public void onItemClick (AdapterView < ? > adapterView, View view,int i, long l){
-//            Intent intent = new Intent( Dogtypes.this, PetActivity.class);
-//            startActivity(intent);
-//
-//        }
+
     }
