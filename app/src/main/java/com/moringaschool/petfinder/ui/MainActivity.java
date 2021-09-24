bb32dcd2c5579143b975505b3473b8e8835debe7
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-09-24 10:07:38 +0300

    modify android manifest

--- app/src/main/java/com/moringaschool/petfinder/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/petfinder/ui/MainActivity.java
@@ -1,17 +1,24 @@
-package com.moringaschool.petfinder;
+package com.moringaschool.petfinder.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import com.moringaschool.petfinder.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
     @BindView(R.id.petsbutton) Button mPetsButton;
 
     @Override
@@ -20,15 +27,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
         mPetsButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
 //        Toast.makeText(MainActivity.this, "Becka", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(MainActivity.this, PetformActivity.class);
+        Intent intent = new Intent(MainActivity.this, DogActivity.class);
         startActivity(intent);
 
     }
+
+
 }
 
